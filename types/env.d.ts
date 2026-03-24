4dfb1caa6687f21c182903e9537d537cf3119e3e
Princess Pulgo <princess.pulgo@neu.edu.ph>
2026-03-24 21:43:00 +0800

    Create env.d.ts

---
+++ types/env.d.ts
@@ -0,0 +1,12 @@
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  // add other VITE_ env vars as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
